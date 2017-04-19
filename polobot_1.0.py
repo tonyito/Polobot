8f941a404165afefed0c728dfe2b4f2011cfb595
tonyito <tonyitocole@gmail.com>
2017-04-18 22:29:52 -0700

    1.0
    Initial

---
+++ polobot_1.0.py
@@ -0,0 +1,93 @@
+from poloniex import poloniex
+import smtplib
+from email.mime.text import MIMEText
+polo = poloniex('API_KEY_HERE','SECRET_HERE')
+
+#openorders = polo.returnOpenOrders("BTC_NAUT")
+x = 0
+ibalance = polo.returnBalances()
+initialBTC = dict((k, v) for k, v in ibalance.items() if k == 'BTC')
+import time
+starttime=time.time()
+while True:
+    balance = polo.returnBalances()
+    values = dict((k, v) for k, v in balance.items() if k != 'BTC' and k != 'STR' and v!= '0.00000000')
+    btcvalue = dict((k, v) for k, v in balance.items() if k == 'BTC')
+    print(values)
+    print(btcvalue.values())
+    afterValue = btcvalue.values()
+    try:
+        beforeValue
+    except NameError:
+        beforeValue = afterValue
+    else:
+        print "Proceed"
+    #if all coins are on orders
+    if bool(values) == False and afterValue == beforeValue:
+        x = 0
+    #if there is a BUY order that was filled
+    elif bool(values) == True and x == 0:
+        print "Not Empty"
+        gmail_user = 'xxxxx@gmail.com'
+        gmail_password = 'xxxxxxx'
+
+        sent_from = gmail_user
+        to = ['xxxxx@gmail.com']
+        subject = 'A Buy Order Was Filled'
+        body = 'A Buy Order Was Filled'
+
+        email_text = """\
+        From: %s
+        To: %s
+        Subject: %s
+
+        %s
+        """ % (sent_from, ", ".join(to), subject, body)
+
+        try:
+            server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+            server.ehlo()
+            server.login(gmail_user, gmail_password)
+            server.sendmail(sent_from, to, email_text)
+            server.close()
+
+            print 'Email sent for Bought!'
+            x = 1
+
+        except:
+            print 'Something went wrong...'
+    elif afterValue > beforeValue:
+        print "Not Empty"
+        gmail_user = 'xxxxxx@gmail.com'
+        gmail_password = 'xxxxxxx'
+
+        sent_from = gmail_user
+        to = ['xxxxxxxx@gmail.com']
+        subject = 'A Sell Order Was Filled'
+        body = 'A Sell Order Was Filled'
+
+        email_text = """\
+        From: %s
+        To: %s
+        Subject: %s
+
+        %s
+        """ % (sent_from, ", ".join(to), subject, body)
+
+        try:
+            server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+            server.ehlo()
+            server.login(gmail_user, gmail_password)
+            server.sendmail(sent_from, to, email_text)
+            server.close()
+
+            print 'Email sent for Sold!'
+            beforeValue = afterValue
+        except:
+            print 'Something went wrong...'
+
+    #if an email was already sent
+    else:
+        print 'Wait'
+        beforeValue = btcvalue.values()
+    time.sleep(60.0 - ((time.time() - starttime) % 60.0))
---
+++ readme.txt
@@ -0,0 +1,5 @@
+This is an email notifier for Poloniex.
+You must have API access enabled for this to work.
+This script was made to be used with Gmail.
+
+Note: If you cancel any orders, the script will trigger an email (i.e. cancelling a buy looks like you sold, and cancelling a sell looks like you bought.) I hope to fix this in future revisions. 
