67350a2d4279d501806d685b0a1f62e10eb2cf19
tonyito <tonyitocole@gmail.com>
2017-04-19 23:22:45 -0700

    Cleaned Up code
    Cleaned

--- polobot_1.0.py
+++ polobot_1.0.py
@@ -1,17 +1,15 @@
 from poloniex import poloniex
 import smtplib
 from email.mime.text import MIMEText
-polo = poloniex('API_KEY_HERE','SECRET_HERE')
+polo = poloniex('API Key Here','Secret Here')
 
 #openorders = polo.returnOpenOrders("BTC_NAUT")
 x = 0
-ibalance = polo.returnBalances()
-initialBTC = dict((k, v) for k, v in ibalance.items() if k == 'BTC')
 import time
 starttime=time.time()
 while True:
     balance = polo.returnBalances()
-    values = dict((k, v) for k, v in balance.items() if k != 'BTC' and k != 'STR' and v!= '0.00000000')
+    values = dict((k, v) for k, v in balance.items() if k != 'BTC' and v!= '0.00000000')
     btcvalue = dict((k, v) for k, v in balance.items() if k == 'BTC')
     print(values)
     print(btcvalue.values())
@@ -28,11 +26,11 @@ while True:
     #if there is a BUY order that was filled
     elif bool(values) == True and x == 0:
         print "Not Empty"
-        gmail_user = 'xxxxx@gmail.com'
-        gmail_password = 'xxxxxxx'
+        gmail_user = 'xxxxxx@gmail.com'
+        gmail_password = 'Password Here'
 
         sent_from = gmail_user
-        to = ['xxxxx@gmail.com']
+        to = ['xxxxxx@gmail.com']
         subject = 'A Buy Order Was Filled'
         body = 'A Buy Order Was Filled'
 
@@ -56,13 +54,13 @@ while True:
 
         except:
             print 'Something went wrong...'
-    elif afterValue > beforeValue:
+    elif afterValue > beforeValue :
         print "Not Empty"
         gmail_user = 'xxxxxx@gmail.com'
-        gmail_password = 'xxxxxxx'
+        gmail_password = 'PAssword Here'
 
         sent_from = gmail_user
-        to = ['xxxxxxxx@gmail.com']
+        to = ['xxxxxx@gmail.com']
         subject = 'A Sell Order Was Filled'
         body = 'A Sell Order Was Filled'
 
